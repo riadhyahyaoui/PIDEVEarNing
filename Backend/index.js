f42ae7b5d4a7be11afd4e230c1ae8c052c1b18cb
teymour21 <teymour.dridi@esprit.tn>
2022-04-11 23:50:25 +0100

    Socket Added

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,9 @@ const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
+//const http = require("http")
 const cors = require("cors");
+var server = require('http').Server(app);
 
 dotenv.config();
 
@@ -17,7 +19,38 @@ mongoose
     .catch((err) => {
         console.log(err);
     });
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: [ "GET", "POST" ]
+    }
+})
+let x=true;
+io.on("connection", (socket) => {
+    socket.emit("me", socket.id)
+    /*io.of("/").adapter.on("create-room", (room) => {
+        console.log(`room ${room} was created`);
+    });*/
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded");
+        x=false;
+    })
 
+    socket.on("callUser", (data) => {
+
+        io.to(data.userToCall).emit("callUser", {signal: data.signalData, from: data.from, name: data.name})
+
+    })
+
+    socket.on("answerCall", (data) => {
+
+        io.to(data.to).emit("callAccepted", data.signal)
+
+        /*io.of("/").adapter.on("join-room", (room, id) => {
+            console.log(`socket ${id} has joined room ${room}`);
+        });*/
+    })
+})
 app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoute);
@@ -26,6 +59,6 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-app.listen(process.env.PORT || 5000, () => {
+server.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!");
 });
