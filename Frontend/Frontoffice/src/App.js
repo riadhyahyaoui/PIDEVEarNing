ca4151cf1f64a67be7e2ac8da678c40b447e1384
riadhyahyaoui94 <riadh.yahyaouui@gmail.com>
2022-04-11 08:38:13 +0100

    HomePage template Integration

--- Frontend/Frontoffice/src/App.js
+++ Frontend/Frontoffice/src/App.js
@@ -1,41 +1,64 @@
-import React, { useState } from 'react';
-
-import { loadModels } from './helpers/faceApi';
-import { createFaLibrary } from './helpers/icons';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Switch from 'react-switch';
-import Camera from './components/Camera/Camera';
-
+import React, {useState} from 'react';
 import './App.css';
-createFaLibrary();
-loadModels();
-function App() {
-  const [mode, setMode] = useState(false); //true = photo mode; false = video mode
+import styled, {css} from "styled-components";
+import Navbar from "./components/Navbar/Navbar";
+import Intro from "./components/Intro/Intro";
+import Feature from "./components/Feature/Feature";
 
-  return (
-    <div className="App">
-      <header>
-        <div className="App__header">
-          <h1>
-            <span>Ear-ning Emotion Song Recommander</span>
-          </h1>
-          <div className="App__switcher">
-            <FontAwesomeIcon icon="camera" color={mode ? '#007c6c' : '#cccccc'} />
-            <Switch
-              onChange={() => setMode(!mode)}
-              uncheckedIcon={false}
-              checkedIcon={false}
-              checked={!mode}
-              className="App__switcher-switch"
-            />
-            <FontAwesomeIcon icon="video" color={!mode ? '#007c6c' : '#cccccc'} />
-          </div>
-        </div>
-      </header>
-      <Camera photoMode={mode} />
-    </div>
-  );
-}
+const Container = styled.div`
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+`;
+
+const Shape = css`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const IntoShape = styled.div`
+  ${Shape}
+  clip-path: polygon(67% 0, 100% 0%, 100% 100%, 55% 100%);
+  background-color: crimson;
+`;
+
+const FeatureShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0 0, 55% 0%, 33% 100%, 0 100%);
+  background-color: pink;
+`;
+
+const ServiceShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0 0, 33% 0%, 33% 100%, 0 100%);
+  background-color: #f88497;
+`;
+
+const PriceShape = styled.div`
+  ${Shape}
+  clip-path: polygon(33% 0, 100% 0%, 100% 100%, 67% 100%);
+  background-color: crimson;
+`;
+
+const App = () => {
+    const smallScreen = window.screen.width <= 480 ? true : false;
+    return (
+        <>
+            <Container>
+                <Navbar/>
+                <Intro/>
+                <IntoShape/>
+            </Container>
+            <Container>
+                <Feature/>
+                <FeatureShape/>
+            </Container>
+        </>
+    );
+};
 
 export default App;
